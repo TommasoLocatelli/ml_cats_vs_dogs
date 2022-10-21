4938d5a7d83bc13af91b561da5326d106dea6f8b
Tomso <tommaso.locatelli.240397@gmail.com>
2022-10-21 12:00:18 +0200

    feedforward1.0

--- preprocessing_utility.py
+++ preprocessing_utility.py
@@ -27,3 +27,12 @@ def ready_to_be_used_dataset(seed=123, image_squared_size=256):
     normalized_train = train_ds.map(lambda x, y: (normalization_layer(x), y))
     normalized_val = val_ds.map(lambda x, y: (normalization_layer(x), y))
     return normalized_train, normalized_val
+
+#https://cloudxlab.com/assessment/displayslide/5658/converting-tensor-to-image
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
