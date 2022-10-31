6a7cef15f2caa373e0002e708fda503241d5f906
Martin Ranieri <51285505+ranieri-unimi@users.noreply.github.com>
2022-10-31 09:51:51 +0100

    bugfix

--- filter.v3.py
+++ filter.v3.py
@@ -1,8 +1,5 @@
-
 # %%
 from utilities import *
-import matplotlib.pyplot as plt
-import numpy as np
 
 import tensorflow as tf
 from tensorflow import keras
@@ -15,72 +12,64 @@ from keras.utils import *
 
 from tensorflow.errors import InvalidArgumentError
 
-files_to_delete = list()
-
 # %%
 import os
 from tqdm import tqdm
+
 pth = "downloads/CatsDogs/{}"
 data_dir = pathlib.Path(pth.format("tmp/"))
 
+# %%
+size = 128
+batch_size = 1
+
+model = Sequential(
+    [
+        Flatten(input_shape=(size, size, 1)),
+    ]
+)
+
+model.compile(
+    optimizer=Adam(),
+    loss=SparseCategoricalCrossentropy(),
+    metrics=["accuracy"],
+)
+
 
 # %%
-for razza in ['Cats', 'Dogs', ]:
+for razza in [
+    "Cats",
+    "Dogs",
+]:
     for i in tqdm(range(12500)):
-        
-        orig_path = pth.format("")+ razza+f"/{i}.jpg"
-        temp_path = pth.format("tmp/")+razza+f"/{i}.jpg"
+
+        orig_path = pth.format("") + razza + f"/{i}.jpg"
+        temp_path = pth.format("tmp/") + razza + f"/{i}.jpg"
         try:
             os.rename(orig_path, temp_path)
         except:
             print(orig_path, "already deleted")
             continue
 
-        image_squared_size=256
-        batch_size = 1
-        img_height = image_squared_size
-        img_width = image_squared_size
-
         train_ds = tf.keras.utils.image_dataset_from_directory(
             data_dir,
             # validation_split=0.2,
-            color_mode='grayscale',
+            color_mode="grayscale",
             # subset="training",
             # seed=seed,
-            image_size=(img_height, img_width),
+            image_size=(size, size),
             batch_size=batch_size,
-        );
+        )
 
         # normalization_layer = tf.keras.layers.Rescaling(1.0 / 255)
         # normalized_train = train_ds.map(lambda x, y: (normalization_layer(x), y))
 
-        train_ds
-
-        
-        model = Sequential(
-            [
-                Flatten(input_shape=(image_squared_size, image_squared_size, 1)),
-                Dense(10, activation="relu"),
-                Dense(2, activation="softmax"),
-            ]
-        )
-
-        model.compile(
-            optimizer=Adam(),
-            loss=SparseCategoricalCrossentropy(),
-            metrics=["accuracy"],
-        )
-
-        
         try:
             model.fit(train_ds, epochs=1, verbose=0)
         except InvalidArgumentError:
             # print('removing', temp_path)
             # os.remove(temp_path)
-            files_to_delete.append(orig_path)
+            with open("files.to.delete.txt", "a") as f:
+                f.write(orig_path + "\n")
         finally:
             os.rename(temp_path, orig_path)
-        
-with open('files.to.delete.txt', 'wt') as f:
-    f.writelines([l+'\n' for l in files_to_delete])
-        
